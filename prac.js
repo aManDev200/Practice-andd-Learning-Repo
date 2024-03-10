31f900b412ae3a8cf3edc0ad3e44fe04967aa4a9
aman154 <fakex1276@gmail.com>
2024-03-10 16:53:28 +0530

    Added the Solution

--- prac.js
+++ prac.js
@@ -1,38 +1,8 @@
-/* const arr = [5,7,8,9,10,1,21,21,54]
-arr.sort((a,b)=> a-b)
-console.log(arr);  Array Sort */
+const express = require('express')
+const app = express();
+const bodyParser = require('body-parser');
+const port = 3000;
 
-
-
-
-/* 
-function sum(n)
-{
-    return n*n;
-}
-function cube(n)
-{
-    return n*n*n;
-}
-function Overall(n,fn)
-{
-    return fn(n);    
-}
-const a = Overall(5,cube);
-console.log(a);   Callback Funtion Example */
-
-
-
-
-/* function MyOwnTimeout(fn,duration)
-{
-    setTimeout(fn,duration);
-}
-
-MyOwnTimeout(function() {
-    console.log("Hello This is my own timeout");
-},5000)  promise example */
-
-
-
-
+app.listen(port,()=>{
+  console.log("The port is active on port no ${port}");
+})
